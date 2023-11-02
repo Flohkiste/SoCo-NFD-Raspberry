120844f9f9326f7e4e134674c68a7b4693e11ebe
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-11-02 14:32:13 +0100

    added groups and functions for the groups

--- src/start.py
+++ src/start.py
@@ -4,13 +4,55 @@ from soco import SoCo
 from soco.plugins.sharelink import *
 import time
 
-WohnzimmerSub = "192.168.150.34"
-WohnzimmerArc = "192.168.150.28"
-WohnzimmerR = "192.168.150.35"
-WohnzimmerL = "192.168.150.29"
-KücheR = "192.168.150.30"
-KücheL = "192.168.150.36"
-Klavier = "192.168.150.39"
+WohnzimmerSub = SoCo("192.168.150.34")
+WohnzimmerArc = SoCo("192.168.150.28")
+WohnzimmerR = SoCo("192.168.150.35")
+WohnzimmerL = SoCo("192.168.150.29")
+KücheR = SoCo("192.168.150.30")
+KücheL = SoCo("192.168.150.36")
+Klavier = SoCo("192.168.150.39")
+
+Wohnzimmer = [
+    SoCo("192.168.150.34"),
+    SoCo("192.168.150.28"),
+    SoCo("192.168.150.35"),
+    SoCo("192.168.150.29"),
+]
+Küche = [SoCo("192.168.150.30"), SoCo("192.168.150.36"), SoCo("192.168.150.39")]
+
+
+def joinGroups():
+    for x in Wohnzimmer:
+        x += 1
+        if x == 4:
+            break
+
+        Wohnzimmer[x].join(Wohnzimmer[0])
+
+    for x in Küche:
+        x += 1
+        if x == 3:
+            break
+
+        Küche[x].join(Küche[0])
+
+
+def resetGroups():
+    for x in Wohnzimmer:
+        x += 1
+        if x == 4:
+            break
+
+        Wohnzimmer[x].unjoin()
+
+    for x in Küche:
+        x += 1
+        if x == 3:
+            break
+
+        Küche[x].unjoin()
+
+    joinGroups()
 
 
 def valueVolumeChanged(value, direction):
